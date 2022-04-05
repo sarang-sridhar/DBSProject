ddb2524554744e55a4c43466d7ef71817b18c8e3
superbhanu22 <f20201675@pilani.bits-pilani.ac.in>
2022-04-06 03:26:26 +0530

    minor changes in code

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoute({ authState }) {
   return authState ? <Outlet /> : <Navigate to="/" />;
 }
+
+ProtectedRoute.propTypes = {
+  authState: PropTypes.bool
+};
 
 export default ProtectedRoute;
