9c5469c6d8d518a19826e53edf4c8d7e6cb1f04d
sarang sridhar <f20200297@pilani.bits-pilani.ac.in>
2022-04-10 01:24:13 +0530

    changes

--- src/redux/reducers/authReducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = false;
-
-const authReducer = (state = initialState, action) => {
-  if (action.type === 'ISLOGGEDIN') return true;
-  else if (action.type === 'ISLOGGEDOUT') return false;
-  else return state;
-};
-
-export default authReducer;
