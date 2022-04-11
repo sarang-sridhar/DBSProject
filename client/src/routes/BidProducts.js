6c8d8dc0dee67b979bbf476c4e3a16774ac1229e
superbhanu22 <f20201675@pilani.bits-pilani.ac.in>
2022-04-11 19:41:40 +0530

    minor changes

--- client/src/routes/BidProducts.js
+++ client/src/routes/BidProducts.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Card, CardActions, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardActions, CardContent, Typography, Button, TextField } from '@mui/material';
 const BidProducts = () => {
   const location = useLocation();
   return (
     <>
       <Card
         sx={{ minWidth: 300, maxWidth: 300 }}
-        style={{ margin: '30px auto' }}
-        elevation="1"
-        variant="elevation">
+        style={{ margin: '30px auto', border: '1px solid black' }}
+        variant="outlined">
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             ItemName : {location.state.itemName}
@@ -22,9 +21,24 @@ const BidProducts = () => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="large" variant="outlined" fullWidth>
-            Bid
-          </Button>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Enter Amount"
+              variant="outlined"
+              color="warning"
+              type="number"
+              fullWidth
+              onChange={() => {
+                console.log('jo');
+              }}
+            />
+          </div>
+          <div>
+            <Button size="large" variant="contained" color="warning" fullWidth>
+              Bid
+            </Button>
+          </div>
         </CardActions>
       </Card>
     </>
